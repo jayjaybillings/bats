5c65d872c0de18dd2b1f335c3e14b718ed850f7f
Jay Jay Billings <billingsjj@ornl.gov>
2019-01-12 23:07:22 -0500

    Finished rudimentary install script and Fuseki docker file.
    Signed-off-by: Jay Jay Billings <billingsjj@ornl.gov>

--- dockerfiles/buildFusekiImage.sh
+++ dockerfiles/buildFusekiImage.sh
@@ -1,7 +1,20 @@
 #/bin/bash
 
-# Download Fuseki
-wget https://www-us.apache.org/dist/jena/binaries/apache-jena-3.10.0.tar.gz
+FUSEKI_VERSION=3.10.0
+FUSEKI_NAME=apache-jena-fuseki
+FUSEKI_DOWNLOAD_FILE=$FUSEKI_NAME-$FUSEKI_VERSION.tar.gz
+
+# Download Fuseki if it isn't available
+if [ ! -f $FILE ]; then
+	wget https://www-us.apache.org/dist/jena/binaries/$FUSEKI_DOWNLOAD_FILE
+fi
+
+# Unpack the Fuseki files
+tar -xzvf $FUSEKI_DOWNLOAD_FILE
+mv $FUSEKI_NAME-$FUSEKI_VERSION $FUSEKI_NAME
 
 # Build the Docker image
 docker build -t fuseki:latest -f Dockerfile.fuseki .
+
+# Remove the unused Fuseki dir
+rm -rf $FUSEKI_NAME
